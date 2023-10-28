ffa57ac028f38cdf07dccfea9258af489aff98ab
thomas@xza.fr <thomas@xza.fr>
2023-10-28 17:15:11 +0100

    Loading and printing sudoku working. Next for the logic...!

--- 96.c
+++ 96.c
@@ -9,13 +9,16 @@
 #define int64  long long int
 #define uint64 unsigned long long int
 
-#define SUDOKU_QUANTITY 50
+#define SUDOKU_QUANTITY 2
 
 void
 read_file(int8 *storage, int8 *filename);
 
 int
 load_sudoku(int8 *raw_data, int8 *sudoku);
+
+void
+print_sudoku(int8 *sudoku_pt);
 
 int
 main(int argc, char *argv[]) {
@@ -28,25 +31,51 @@ main(int argc, char *argv[]) {
 
   read_file(&raw_data[0], "96_sudoku_set.txt");
 
-  
-  
-  /* for ( i = 0 ; i < SUDOKU_QUANTITY * 81 ; i += 81 ) { */
+  for ( i = 0 ; i < SUDOKU_QUANTITY * 81 ; i += 81 ) {
 
-  /*   /\* load_sudoku(&raw_data[i], &sudoku[0]); *\/ */
+    load_sudoku(&raw_data[i], &sudoku[0]);
 
-  /*   printf("nothing"); */
+    /* print_sudoku(&sudoku[0]); */
 
-  /* } */
+  }
 
   return 0;
 
 }
 
 
-int
-load_sudoku(int8 *raw_data, int8 *sudoku) {
+void
+print_sudoku(int8 *sudoku_pt) {
 
+  int i;
+
+  for ( i = 0 ; i < 81 ; i++ ) {
+
+    printf("%d ", *(sudoku_pt + i));
+
+    if ( (i+1) % 9 == 0 ) {
+
+      printf("\n");
+
+    }
+
+  }
+
+  printf("\n");
   
+}
+
+
+int
+load_sudoku(int8 *raw_data_pt, int8 *sudoku_pt) {
+
+  int i;
+
+  for ( i = 0 ; i < 81 ; i++ ) {
+
+    *(sudoku_pt + i) = *(raw_data_pt + i);
+
+  }
 
 }
 
@@ -70,9 +99,7 @@ read_file(int8 *storage, int8 *filename_pt) {
 
     if (char_n >= 0 && char_n <= 9) {
       
-      /* printf(" %c %d %i \n", char_read, char_n, i); */
-
-      *(storage + i) = 1;
+      *(storage + i) = char_n;
 
       i += 1;
 
